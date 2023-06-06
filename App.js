b0975ec998a19f06ad5a40455638e7e7c6d435cd
Lucija Krsticevic <lkrstice1@pmfst.hr>
2023-06-06 21:37:21 +0200

    Izgled aplikacije

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import PocetniEkran from './screens/PocetniEkran';
+import DetaljEkran from './screens/DetaljEkran';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name='Naslovna' 
+          component={PocetniEkran} 
+          options={{
+            title: 'Moja naslovnica',
+            headerStyle: {
+              backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+        <Stack.Screen 
+          name='Detalji' 
+          component={DetaljEkran}
+          initialParams={{ broj: 42, poruka: "Nema poruke" }}
+          options={({ navigation, route }) => {
+            return{ title: route.params.ime}
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
